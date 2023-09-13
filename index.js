6e0ab9c5f6c366225c6fad7f40998aa83c97bdb3
mephaline <suaramos88@gmail.com>
2023-09-12 21:34:19 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    // Assuming you have a maximum width for the container (e.g., 400px)
+    const maxWidth = 400; // Change this value to your container's maximum width
+  
+    if (left < maxWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
